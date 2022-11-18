c50a42d9a206c3544833bc5522bc91ccd3edcc53
1007446687 <andres.montana.r4@gmail.com>
2022-11-18 15:21:46 -0500

    eliminado archivos

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import './App.css';
 import Header from "./componentes/Header";
 import Footter from "./componentes/Footer";
 import Index from "./pages/Index"
-import DetalleP from './pages/Detalle';
 import { Route, Routes } from "react-router-dom"
-import DashboardP from './pages/Dashboard';
 function App() {
   return (
 
@@ -12,8 +10,6 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Index />}/>
-        <Route path="/detalle/:id" element={<DetalleP/>}/>
-        <Route path="/dashboard" element={<DashboardP/>}/>
       </Routes>
       <Footter />
 
